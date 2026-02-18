8e4f0787bcaca972c9e3ad223c104c3639727515
hanssystem <hanssystem1@gmail.com>
2026-02-18 21:43:06 +0100

    ajout de newlester

--- script.js
+++ script.js
@@ -19,10 +19,10 @@ rot =document.getElementById("change");
 fond.addEventListener("click",()=>{
     navi.classList.toggle("active");
     rot.classList.toggle("rotate");
-    document.querySelector("footer").classList.toggle("fond");
-    document.querySelector("ion-icon").classList.toggle("active");
+    document.querySelector("footer").classList.toggle("foot");
+    document.querySelector(" em ").classList.toggle("blanc");
+    document.querySelector(" a ").classList.toggle("blanc");
     all.classList.toggle("active");
-    // burger.classList.;
     
 });
 aff=document.querySelector("ul");
@@ -31,28 +31,3 @@ menu.addEventListener("click",()=>{
     aff.classList.toggle("present");
     document.querySelector("nav .option").classList.toggle("present");
 })
-function rotate() {
-    // rot = document.getElementById('change')
-    // rot.style.transform = 'rotate(180deg)';
-    // rot.style.transition = 'transform  1s ease-out';
-    // document.getElementById('change').addEventListener(
-    //     "click", () => {
-    //         n=100;
-    //         i=0;
-    //         if(i<=n)
-    //         {rot.style.transform = 'rotate(360deg)';
-    //         rot.style.transition = 'transform  1s ease infinite';}
-    //     }
-    // );
-}
-// rot = document.getElementById('change');
-// rot.style.color = 'white';
-
-// document.getElementById('change').addEventListener(
-//     "click" ,()=>{
-//         rot = document.getElementById('change');
-//         rot.style.transform = "rotate('360deg')";
-
-
-//     }
-// );
