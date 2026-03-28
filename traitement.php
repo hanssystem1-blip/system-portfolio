24bc34461c5ff02134f506697ceb4411d8d7fd18
hanssystem <hanssystem1@gmail.com>
2026-03-28 16:08:46 +0100

    finition

--- traitement.php
+++ traitement.php
@@ -1,5 +1,14 @@
 <?php
-
+$connexion= mysqli_connect("localhost","" ,"root" ,"system");
+if (isset($_POST["username"])&&isset($_POST["email"])&&isset($_POST["messsage"])) {
+    $username=$_POST["username"];
+    $email=$_POST["email"];
+    $message=$_POST["message"];
+    if(!(empty($email)&&empty($username)&&empty($message))){
+        $req="insert into recommandation(username,email,message)values('$usename','$email','$message') ";
+        $sql=mysqli_query($connexion,$req);
+    }
+}
 header("location:index.html");
 
 
